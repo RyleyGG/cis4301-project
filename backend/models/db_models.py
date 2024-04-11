37d8cc657f7e15ff4e935fcd61019cc96164a1f0
RyleyGG <griffithryley2000@gmail.com>
2024-04-11 19:31:51 -0400

    updates to data models and implement etl process

--- backend/models/db_models.py
+++ backend/models/db_models.py
@@ -2,7 +2,7 @@ from sqlmodel import SQLModel, Field
 from typing import Optional
 from datetime import datetime
 import uuid
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, ForeignKeyConstraint
 
 
 class Dummy(SQLModel, table=True):
@@ -14,42 +14,45 @@ class Dummy(SQLModel, table=True):
 class FireIncident(SQLModel, table=True):
     __tablename__ = 'FireIncident'
     id: int = Field(primary_key=True)
-    cause_code: str = Field(sa_column=Column(String(255)))
-    cause_description: str = Field(sa_column=Column(String(255)))
-    discovery_datetime: datetime = Field()
-    containment_datetime: datetime = Field()
+    cause_code: float = Field()
+    cause_description: str = Field(sa_column=Column(String(100)))
+    discovery_datetime: Optional[datetime] = Field(default=None)
+    containment_datetime: Optional[datetime] = Field(default=None)
     size_acres: float = Field()
-    size_category: str = Field(sa_column=Column(String(50)))
+    size_category: str = Field(sa_column=Column(String(1)))
     year_of_fire: int = Field()
-    fips_name: str = Field(sa_column=Column(String(255)))
-    fips_code: str = Field(sa_column=Column(String(10)))
+    fips_name: Optional[str] = Field(default=None, sa_column=Column(String(255)))
+    fips_code: Optional[str] = Field(default=None, sa_column=Column(String(255)))
     longitude: float = Field()
     latitude: float = Field()
-    fire_name: str = Field(sa_column=Column(String(255)))
-    fire_code: str = Field(sa_column=Column(String(50)))
-    fire_project_id: int = Field()
-    agency_code_id: Optional[str] = Field(
-        default=None,
-        sa_column=Column(String(255), ForeignKey('ReportingAgency.agency_code'))
+    fire_name: Optional[str] = Field(default=None, sa_column=Column(String(255)))
+    fire_code: Optional[str] = Field(default=None, sa_column=Column(String(10)))
+    agency_code_id: Optional[str] = Field(default=None, sa_column=Column(String(255)))
+    reporting_unit_id: Optional[str] = Field(default=None, sa_column=Column(String(255)))
+
+    __table_args__ = (
+        ForeignKeyConstraint(['agency_code_id', 'reporting_unit_id'],
+                             ['ReportingAgency.agency_code', 'ReportingAgency.reporting_unit_id']),
     )
 
 
 class ReportingAgency(SQLModel, table=True):
     __tablename__ = 'ReportingAgency'
     agency_code: str = Field(sa_column=Column(String(255), primary_key=True))
-    reporting_unit_id: Optional[int] = Field(default=None, foreign_key='NWCGUnit.unit_id')
-    reporting_unit_code: str = Field(sa_column=Column(String(50)))
+    # This is purposefully not a foreign key to NWCGUnit to allow us to store data
+    # on agencies that may not have associated NWCGUnit entries
+    reporting_unit_id: str = Field(sa_column=Column(String(255), primary_key=True))
+    reporting_unit_name: str = Field(sa_column=Column(String(255)))
 
 
 class NWCGUnit(SQLModel, table=True):
     __tablename__ = 'NWCGUnit'
-    unit_id: int = Field(default=None, primary_key=True)
-    parent_agency: str = Field(sa_column=Column(String(255)))
+    unit_id: str = Field(sa_column=Column(String(255), primary_key=True))
+    parent_agency: Optional[str] = Field(default=None, sa_column=Column(String(255)))
     agency_name: str = Field(sa_column=Column(String(255)))
-    department_or_state: str = Field(sa_column=Column(String(255)))
+    department_or_state: Optional[str] = Field(default=None, sa_column=Column(String(255)))
     wildland_role: str = Field(sa_column=Column(String(50)))
     geographic_area_code: str = Field(sa_column=Column(String(50)))
-    coordination_center_code: str = Field(sa_column=Column(String(50)))
     unit_name: str = Field(sa_column=Column(String(255)))
     unit_type: str = Field(sa_column=Column(String(50)))
     unit_code: str = Field(sa_column=Column(String(50)))
