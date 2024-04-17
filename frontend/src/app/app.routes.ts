87d0b33337aba60a03da0ed9f5e899d66ac23fd2
RyleyGG <griffithryley2000@gmail.com>
2024-04-17 00:43:05 -0400

    add frontend for db status

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,18 @@
 import {Routes} from '@angular/router';
 import {AppComponent} from './app.component';
 import {LandingPageComponent} from './pages/landing/landing.page.component';
-import { WildfiresChangesComponent } from './pages/WildfireChangesInSizeAndFrequency/wildfires-changes/wildfires-changes.component';
-import { WildfireTypesBasedOnGeographyComponent } from './pages/wildfire-types-based-on-geography/wildfire-types-based-on-geography.component';
-import { WildfireSizesBasedOnGeographyComponent } from './pages/wildfire-sizes-based-on-geography/wildfire-sizes-based-on-geography.component';
-import { AgencyContainmentTimeComponent } from './pages/agency-containment-time/agency-containment-time.component';
-import { SizeOfWildfireTypesComponent } from './pages/size-of-wildfire-types/size-of-wildfire-types.component';
+import {
+  WildfiresChangesComponent
+} from './pages/WildfireChangesInSizeAndFrequency/wildfires-changes/wildfires-changes.component';
+import {
+  WildfireTypesBasedOnGeographyComponent
+} from './pages/wildfire-types-based-on-geography/wildfire-types-based-on-geography.component';
+import {
+  WildfireSizesBasedOnGeographyComponent
+} from './pages/wildfire-sizes-based-on-geography/wildfire-sizes-based-on-geography.component';
+import {AgencyContainmentTimeComponent} from './pages/agency-containment-time/agency-containment-time.component';
+import {SizeOfWildfireTypesComponent} from './pages/size-of-wildfire-types/size-of-wildfire-types.component';
+import {DatabaseStatusComponent} from "./pages/db-status/database-status.page.component";
 
 
 export const routes: Routes = [
@@ -17,5 +24,6 @@ export const routes: Routes = [
   {path: "wildfire-types-geography", component: WildfireTypesBasedOnGeographyComponent},
   {path: "containment-vs-size", component: AgencyContainmentTimeComponent},
   {path: "wildfire-types-size", component: SizeOfWildfireTypesComponent},
+  {path: "status", component: DatabaseStatusComponent},
   {path: '**', component: AppComponent} // TODO: make this a PageNotFound or 404 error page
 ];
