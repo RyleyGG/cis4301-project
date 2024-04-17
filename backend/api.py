ba58f8c5786994b5deb2aea05a1d4c8bcc64f196
RyleyGG <griffithryley2000@gmail.com>
2024-04-17 00:43:19 -0400

    add db status endpoint

--- backend/api.py
+++ backend/api.py
@@ -1,8 +1,10 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy import func
 from sqlmodel import SQLModel, Session, select
-from models.db_models import Dummy
+from models.db_models import Dummy, FireIncident, NWCGUnit, ReportingAgency
+from models.dto_models import TblSizeResp
 
 from routers import dummy_router, fire_incident_router, trend_router
 from services.api_utility_service import engine, get_session
@@ -31,7 +33,7 @@ app.add_middleware(
 )
 
 
-@app.get("/write")
+@app.get('/write')
 async def root(db: Session = Depends(get_session)):
     newDummy = Dummy(some_str='Hello world')
     db.add(newDummy)
@@ -40,7 +42,25 @@ async def root(db: Session = Depends(get_session)):
     return {'message': 'Hello World'}
 
 
-@app.get("/read")
+@app.get('/read')
 async def root(db: Session = Depends(get_session)):
     dummy_val = db.exec(select(Dummy)).first()
     return {'message': dummy_val.some_str}
+
+
+@app.get('/db_status', response_model=TblSizeResp, response_model_by_alias=False)
+async def get_db_status(db: Session = Depends(get_session)):
+    fire_count = select(func.count()).select_from(FireIncident)
+    nwcg_count = select(func.count()).select_from(NWCGUnit)
+    reporting_agency_count = select(func.count()).select_from(ReportingAgency)
+    fire_size = db.exec(fire_count).one()
+    ncwg_size = db.exec(nwcg_count).one()
+    reporting_agency_size = db.exec(reporting_agency_count).one()
+    total = fire_size + ncwg_size + reporting_agency_size
+
+    return TblSizeResp(
+        fire_incident_size=fire_size,
+        nwcg_unit_size=ncwg_size,
+        reporting_agency_size=reporting_agency_size,
+        total_size=total
+    )
--- backend/models/dto_models.py
+++ backend/models/dto_models.py
@@ -7,3 +7,10 @@ class FireIncidentFilters(BaseModel):
     size_category: Optional[str] = None
     year_of_fire_max: Optional[int] = None
     year_of_fire_min: Optional[int] = None
+
+
+class TblSizeResp(BaseModel):
+    fire_incident_size: int
+    nwcg_unit_size: int
+    reporting_agency_size: int
+    total_size: int
